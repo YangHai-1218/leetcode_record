048f52f643b6cb2d321a47010397067107b21da7
NaCl-Ocean <Yanghai1218@outlook.com>
2020-10-20 18:26:25 +0800

    init commit

---
+++ 1.两数之和.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums, target):
+        temp = {}
+        for i, num in enumerate(nums):
+            if target - num in temp:
+                return [temp[target-num], i]
+            temp[num] = i
+        return None
+# @lc code=end
+
